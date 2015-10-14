72d30d06ed482e834e014a13d7a63790893ad95e
Simon Heumos <heumos.simon@gmail.com>
2015-10-14 15:14:55 +0200

    Started 'Getting Started'

---
+++ src/Nucleotide.java
@@ -0,0 +1,21 @@
+/**
+ * Created by heumos on 14.10.15.
+ *
+ * Java class representing a nucleotide.
+ * Nucleotides (allowed) are: https://en.wikipedia.org/wiki/Nucleotide
+ *
+ */
+public class Nucleotide {
+
+    private String base;
+
+    /**
+     * A nucleotide consists of one base.
+     * @param base the nucleotide given as a String
+     */
+    public Nucleotide(String base) {
+        this.base = base;
+    }
+
+
+}
